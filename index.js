f3977deda53f163f87a2603a2efc3a787a052bc0
LópezBarranco <604270@cognizant.com>
2019-05-31 13:49:10 +0200

    Add fields filter. Add get by id

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require('express');
-const simpleIcons = require('simple-icons');
+const simpleIcons = Object.values(require('simple-icons')).map(e => Object.assign({}, e, { id: e.title.toLowerCase().replace(/ /g, '-') }));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/icons', (req, res) => {
+
+const filterObjByKeys = obj => keysToFilter => keysToFilter.map(k => k in obj ? { [k]: obj[k] } : {})
+    .reduce((res, o) => Object.assign(res, o), {});
+
+app.get('/', (req, res) => {
     const searchStr = req.query.q;
+    const fields = (req.query.fields && req.query.fields.split(',')) || ['id', 'title', 'hex'];
     const color = req.query.color;
-    const icons = Object.entries(simpleIcons)
-                    .map(e => ({ id: e[0], title: e[1].title, hex: e[1].hex }))
-                    .filter(e => e.title.match(new RegExp(searchStr, 'i')))
-                    .filter(e => !color || (e.hex.toLowerCase() === color.toLowerCase()))
+
+    const icons = simpleIcons
+        .filter(e => e.title.match(new RegExp(searchStr, 'i')))
+        .filter(e => !color || (e.hex.toLowerCase() === color.toLowerCase()))
+        .map(e => filterObjByKeys(e)(fields))
     res.send(JSON.stringify(icons));
+});
+
+app.get('/:id', (req, res) => {
+    const fields = (req.query.fields && req.query.fields.split(',')) || ['id', 'title', 'hex'];
+
+    const icon = simpleIcons.find(e => e.id === req.params.id);
+    const formattedIcon = icon ? filterObjByKeys(icon)(fields) : null;
+
+    res.send(JSON.stringify(formattedIcon));
 });
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
